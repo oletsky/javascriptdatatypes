1316c399a258dc4fe1ff65310fc285e9c25571c0
Oleksiy Oletsky <oletsky@ukr.net>
2020-09-01 09:53:15 +0300

    Add files via upload

---
+++ demotypeof.js
@@ -0,0 +1,11 @@
+"use strict";
+let a=1;
+console.log(typeof a); //number
+let s='1';
+console.log(typeof s); //string
+let arr=[1,2,3];
+console.log(typeof arr); //object
+let nl=null;
+console.log(typeof nl); //object
+let und=undefined;
+console.log(typeof und); //undefined
---
+++ implconvers.js
@@ -0,0 +1,5 @@
+"use strict";
+let s=5;
+console.log("Type of "+s+ " is "+typeof(s));//number
+s+=" points";
+console.log("Type of "+s+ " is "+typeof(s));//string
